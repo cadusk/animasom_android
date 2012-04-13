46f759e0219e6334e7097d7179d4b91733085d5b
Carlos Cardoso <cadusk@yahoo.com.br>
2012-04-12 21:59:20 -0300

    Animacao de imagens no click

--- src/br/com/programmers/animasom_alegria/AnimasomActivity.java
+++ src/br/com/programmers/animasom_alegria/AnimasomActivity.java
@@ -2,12 +2,36 @@ package br.com.programmers.animasom_alegria;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 public class AnimasomActivity extends Activity {
+	private boolean _flip = false;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.main);        
+    }
+    
+    public void ClickHandler(View view) {
+    	
+        View visivel;
+        View escondida;
+
+    	if (this._flip)
+    	{
+    		escondida = (View)this.findViewById(R.id.imageView1);
+			visivel = (View)this.findViewById(R.id.imageView2);
+    	}
+    	else
+    	{
+    		escondida = (View)this.findViewById(R.id.imageView2);
+			visivel = (View)this.findViewById(R.id.imageView1);    		
+    	}
+    	
+    	visivel.setVisibility(View.VISIBLE);
+    	escondida.setVisibility(View.GONE);
+    	this._flip = !this._flip;
     }
 }
