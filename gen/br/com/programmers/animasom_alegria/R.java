aefe1c3a5014b460d2e781dd44bf8470c0dffa18
Carlos Cardoso <cadusk@yahoo.com.br>
2012-04-12 21:37:03 -0300

    Configuracao de layout.

--- gen/br/com/programmers/animasom_alegria/R.java
+++ gen/br/com/programmers/animasom_alegria/R.java
@@ -15,11 +15,14 @@ public final class R {
         public static final int alegria2=0x7f020001;
         public static final int ic_launcher=0x7f020002;
     }
+    public static final class id {
+        public static final int imageView1=0x7f050000;
+        public static final int imageView2=0x7f050001;
+    }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static final int app_name=0x7f040000;
     }
 }
