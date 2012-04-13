46194abca33239a90b4e4e85004bc6b1aefe3854
Carlos Cardoso <cadusk@yahoo.com.br>
2012-04-12 21:25:06 -0300

    Initial commit

---
+++ gen/br/com/programmers/animasom_alegria/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package br.com.programmers.animasom_alegria;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/br/com/programmers/animasom_alegria/AnimasomActivity.java
@@ -0,0 +1,13 @@
+package br.com.programmers.animasom_alegria;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AnimasomActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
